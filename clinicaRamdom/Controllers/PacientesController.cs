670f00bfd681ef0b27ab1b81918f3609ec7ac4b7
Diegoxd12wee436 <pirataxd59@gmail.com>
2026-09-12 11:52:39 -0600

    Primer commit

---
+++ clinicaRamdom/Controllers/PacientesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace clinicaRamdom.Controllers
+{
+    public class PacientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
